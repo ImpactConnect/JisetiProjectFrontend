60bf99765c9ce3b6ba6692cf008093698918e697
Mheyrie <omota4fidelity@gmail.com>
2023-12-12 14:59:54 +0100

    Loginform updated

--- client/src/component/LoginForm.js
+++ client/src/component/LoginForm.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import './LoginForm.css'
-import { FaUser } from "react-icons/fa"
-import { FaLock } from "react-icons/fa"
+import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator'
 
 function LoginForm() {
   return (
     <div className="login_container">
-      <form action="" className="login_form">
+      <center>
         <h1 className="login">Login</h1>
-        <div className="input_box">
-          <input type="text" placeholder="Enter Username/Email" required />
-          <FaUser className="icon"/>
-        </div>
-        <div className="input_box"> 
-          <input type="password" placeholder="Enter Password" required />
-          <FaLock className="icon"/>
+        <div className="login_form">
+          <ValidatorForm className="input_box">
+            <TextValidator type="text" name="Email/Username" className="email_input" label="Enter Username/Email" validators={["required"]}></TextValidator>
+            <TextValidator type="password" name="Password" className="pwd_input" label="Enter Password" validators={["required"]} ></TextValidator>
+          </ValidatorForm>
         </div>
         <div className="remember_forgot">
-          <label htmlFor=""><input type="checkbox" />Remember me?</label>
+          <label htmlFor=""><input type="checkbox"/>Remember me?</label>
           <a href="#">Forgot Password?</a>
         </div>
         <button type="submit" className="submit">Login</button>
         <div className="register_link">
           <p>Don't have an account? <a href="#">Register</a></p>
         </div>
-      </form>
+      </center>
+      
     </div>
   )
 }
