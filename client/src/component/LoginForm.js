2b932d2320855a9ba1bd7268aed10a817bcd7e42
Mheyrie <omota4fidelity@gmail.com>
2023-12-12 04:10:22 +0100

    LoginForm CSS added

--- client/src/component/LoginForm.js
+++ client/src/component/LoginForm.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import { FaUser } from "react-icons/fa";
-import { FaLock } from "react-icons/fa";
+import './LoginForm.css'
+import { FaUser } from "react-icons/fa"
+import { FaLock } from "react-icons/fa"
 
 function LoginForm() {
   return (
     <div className="login_container">
-      <form action="">
-        <h1>Login</h1>
+      <form action="" className="login_form">
+        <h1 className="login">Login</h1>
         <div className="input_box">
           <input type="text" placeholder="Enter Username/Email" required />
-          <FaUser />
-          <br/>
+          <FaUser className="icon"/>
+        </div>
+        <div className="input_box"> 
           <input type="password" placeholder="Enter Password" required />
-          <FaLock />
+          <FaLock className="icon"/>
         </div>
         <div className="remember_forgot">
           <label htmlFor=""><input type="checkbox" />Remember me?</label>
           <a href="#">Forgot Password?</a>
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" className="submit">Login</button>
         <div className="register_link">
-          <o>Don't have an account? <a href="#">Register</a></o>
+          <p>Don't have an account? <a href="#">Register</a></p>
         </div>
       </form>
     </div>
--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -7,27 +7,20 @@ function NavBar() {
   return (
     <div className="navbar_container">
       <nav className="navbar_nav">
-        <img src={logo}alt="pic"/>
+        <img src={logo}alt="pic" className="logo_image"/>
         <span className="navbar_ul">
           <ul className="navbar_links">
             {/* <li><Link to="home" spy={true} smooth={true} offset={50} duration={500} >Home</Link></li>
             <li><Link to="about" spy={true} smooth={true} offset={50} duration={500} >About</Link></li>
             <li><Link to="contact" spy={true} smooth={true} offset={50} duration={500} >Contact</Link></li> */}
             
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink exact to="/about">About</NavLink></li>
-            <li><NavLink exact to="/contact">Contact</NavLink></li>
+            <li><NavLink className="home_home" exact to="/">Home</NavLink></li>
+            <li><NavLink className="about_about" exact to="/about">About</NavLink></li>
+            <li><NavLink className="contact_contact" exact to="/contact">Contact</NavLink></li>
             {/* <li><NavLink exact to="/post">Post</NavLink></li> */}
-            <li><NavLink exact to="/login">Login</NavLink></li>
-            <li><NavLink exact to="/register">Register</NavLink> </li>
-             
-             
-             
-             
-             
-            
+            <li><NavLink className="login_login" exact to="/login">Login</NavLink></li>
+            <li><NavLink className="register_register" exact to="/register">Register</NavLink> </li>
           </ul>
-           
         </span>
       </nav>      
     </div>
