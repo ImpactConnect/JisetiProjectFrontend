434849c0bcf347baf5e56c24285522516f5ec691
Mheyrie <omota4fidelity@gmail.com>
2023-12-13 08:21:45 +0100

    CSS updated

--- client/src/component/Contact.js
+++ client/src/component/Contact.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { FaFacebook } from "react-icons/fa";
-import { BsTwitterX } from "react-icons/bs";
-import { FcGoogle } from "react-icons/fc";
-import { IoLogoInstagram } from "react-icons/io5";
+import React from 'react'
+import './Contact.css'
+import { FaFacebook } from "react-icons/fa"
+import { BsTwitterX } from "react-icons/bs"
+import { FcGoogle } from "react-icons/fc"
+import { IoLogoInstagram } from "react-icons/io5"
 
 
 
@@ -10,19 +11,21 @@ function Contact() {
   return (
     <footer className="footer_container" id="contact">
       <h3>Contact Us:</h3>
-      <div className="footer_icons_container">
-        <ul className="footer_icon">
-          <li><a href="#"><FaFacebook /></a></li>
-          <li><a href="#"><BsTwitterX /></a></li>
-          <li><a href="#"><FcGoogle /></a></li>
-          <li><a href="#"><IoLogoInstagram /></a></li>
-        </ul>
-      </div>
-      <div className="footer_text_container">
-        <p className="footer_text">
-          Copyright &copy; 2017 All Rights Reserved by <a href="#h">JISETI</a>.
-        </p>
-      </div>
+      <center>
+        <div className="footer_icons_container">
+          <ul className="footer_icon">
+            <li><a href="#"><FaFacebook /></a></li>
+            <li><a href="#"><BsTwitterX /></a></li>
+            <li><a href="#"><FcGoogle /></a></li>
+            <li><a href="#"><IoLogoInstagram /></a></li>
+          </ul>
+        </div>
+        <div className="footer_text_container">
+          <p className="footer_text">
+            Copyright &copy; 2017 All Rights Reserved by <a href="#h">JISETI</a>.
+          </p>
+        </div>
+      </center>
     </footer>
   )
 }
--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './NavBar.css'
 import {Link} from "react-scroll"
 import { NavLink } from "react-router-dom"
 import logo from "./images/logo.png"
